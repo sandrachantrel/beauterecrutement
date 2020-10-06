fc4da771f571b8ee8d9bf62978ddca952b949cda
sandrachantrel <sandrachantrel@outlook.com>
2020-10-06 17:58:09 +0200

    Création du composant HeaderLayout

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HeaderLayout />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderLayout from "./components/HeaderLayout";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HeaderLayout
   }
 };
 </script>
---
+++ app/src/components/HeaderLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="header">
+        <section class="header__elements">
+        <img class="logo" src="/images/logo_facebook.jpg" alt="Logo Beauté Recrutement">
+        <nav class="menu">
+            <a class="menu__link" href="">Accueil</a>
+            <a class="menu__link" href="">Tarifs</a>
+            <a class="menu__link" href="">Contact</a>
+            <a class="menu__link" href="../integration/login.html">Espace Recruteurs</a>
+        </nav>
+        </section>
+        <div class="content">
+            <span class="first-part">Et si le recrutement commençait ici?</span>
+            <span class="second-part">Logiciel RH pour les entreprises du Bien-Être et de la Beauté</span>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderLayout"
+};
+</script>
+
+<style>
+
+</style>
