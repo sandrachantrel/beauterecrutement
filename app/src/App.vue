fbc0944b5741d4d523655ffc5d3bd026d1bcc5f3
sandrachantrel <sandrachantrel@outlook.com>
2020-10-08 21:02:55 +0200

    installation vue-router

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="app">
-    <HeaderLayout />
-    <OfferList />
-    <FooterLayout />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HeaderLayout from "./components/HeaderLayout";
-import OfferList from "./components/OfferList";
-import FooterLayout from "./components/FooterLayout";
-
-export default {
-  name: "App",
-  components: {
-    HeaderLayout,
-    OfferList,
-    FooterLayout
-  }
-};
-</script>
-
 <style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
