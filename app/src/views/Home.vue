7fbb93e6ad95a8b6bd859a145f74328211e635c3
sandrachantrel <sandrachantrel@outlook.com>
2020-10-09 11:45:23 +0200

    integration du composant HomeLayout à la vue Home

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
-    <h1>PRESENTATION</h1>
+  <HomeLayout />
 </template>
 
 <script>
+import HomeLayout from "@/components/HomeLayout.vue";
 
 export default {
   name: "Home",
   components: {
-    
+    HomeLayout
   }
 };
 </script>
